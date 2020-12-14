9b21f2daca8a8cc3fecf802aefa7da4b6c51af54
Roj <n00b@localhost:5931>
2020-12-14 22:28:39 +0300

    Add TTS Module

---
+++ userbot/plugins/tts.py
@@ -0,0 +1,31 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from userbot import UserBot
+from userbot.plugins.help import add_command_help
+from gtts import gTTS, lang
+import os
+
+LANGS = lang.tts_langs()
+
+
+@UserBot.on_message(filters.command('gtts', ['.']) & filters.me)
+async def send_query(bot: UserBot, message: Message):
+    try:
+        i = message.text[6:]
+        l = i.split()[0] if i.split()[0] in LANGS else "en"
+        t = i if i.split()[0] not in LANGS else i[len(l) + 1:]
+        gTTS(t, lang=l).save("tts.mp3")
+        reply = message.reply_to_message.message_id if message.reply_to_message_id else None
+        await message.delete()
+        await UserBot.send_voice(message.chat, open("tts.mp3", "rb"), reply_to_message_id=reply)
+    except:
+        pass
+
+add_command_help(
+    'tts', [
+        [
+            '.gtts',
+            'بە دەنگ کردنی نووسینەکەت'
+        ]
+    ]
+)
