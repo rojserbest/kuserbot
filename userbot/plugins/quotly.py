ea1738e67aa3cb1d9aa31c072001fc5bf5421d8f
Roj <49933115+rojserbest@users.noreply.github.com>
2020-12-14 20:56:40 +0300

    Create quotly.py

---
+++ userbot/plugins/quotly.py
@@ -0,0 +1,45 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from userbot import UserBot
+from userbot.plugins.help import add_command_help
+
+CHAT_ID = None
+REPLY_TO = None
+
+@Userbot.on_message(filters.user("QuotLyBot") & filters.sticker)
+async def send_result(bot: UserBot, message: Message):
+    global CHAT_ID, REPLY_TO
+    
+    if CHAT_ID != None:
+        try:
+            await UserBot.send_message(CHAT_ID, message.sticker.file_id, reply_to_message_id=REPLY_TO)
+            CHAT_ID, REPLY_TO = None, None
+        except:
+            pass
+
+
+@UserBot.on_message(filters.command('quotly', ['.']) & filters.me)
+async def send_query(bot: UserBot, message: Message):
+    global CHAT_ID, REPLY_TO
+    
+    try:
+        CHAT_ID, REPLY_TO = message.chat.id, message.reply_to_message.id if message.reply_to_message else None
+        await UserBot.send_message("QuotLyBot", message.text[8:])
+        await message.delete()
+    except:
+        pass
+
+    try:
+        await message.delete()
+    except:
+        pass
+
+
+add_command_help(
+    'quotly', [
+        [
+            '.quotly',
+            'بە وتە کردنی نووسینەکەت'
+        ]
+    ]
+)
